6ec2cd7eeaed7cc148969ff006e39dd2a31801a7
pranay saha <pranaysaha1994@gmail.com>
2018-01-31 13:28:33 +0530

    Authentication

---
+++ app/src/main/java/com/example/dell/commonroom/AuthActivity.java
@@ -0,0 +1,148 @@
+package com.example.dell.commonroom;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private EditText mPhoneText;
+    private EditText mCodeText;
+    private Button mbtn;
+
+    private TextView mErrorText;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
+
+    private String mVerificationId;
+
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        mPhoneText=(EditText)findViewById(R.id.auth_phone);
+        mCodeText=(EditText)findViewById(R.id.auth_Verification);
+
+       // mErrorText=(TextView) findViewById(R.id.errorText);
+
+        mbtn=(Button)findViewById(R.id.auth_button);
+
+
+
+
+        mAuth= FirebaseAuth.getInstance();
+
+        mbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mPhoneText.setEnabled(false);
+                mbtn.setEnabled(false);
+               // mCodeText.setEnabled(false);
+
+                String phonenumber=mPhoneText.getText().toString();
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phonenumber,
+                        60,
+                        TimeUnit.SECONDS,
+                        AuthActivity.this,
+                                mCallBacks
+                );
+
+            }
+        });
+
+        mCallBacks= new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+
+
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+
+                mErrorText.setText("There Was Some Error In Verificatrion");
+                mErrorText.setText(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                mCodeText.setVisibility(View.VISIBLE);
+                mbtn.setText("Verify Code");
+
+                // ...
+            }
+
+        };
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+
+                            FirebaseUser user = task.getResult().getUser();
+
+                            Intent subIntent=new Intent(AuthActivity.this,MainActivity.class);
+                            startActivity(subIntent);
+                            finish();
+                            // ...
+                        } else {
+                            // Sign in failed, display a message and update the UI
+
+                            mErrorText.setText("There Was Some Error In Loggin In");
+                            mErrorText.setText(View.VISIBLE);
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+
+}
+
---
+++ app/src/main/java/com/example/dell/commonroom/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.dell.commonroom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+
+    private Button mlogout;
+private Toolbar mtoolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //mtoolbar=(Toolbar)findViewById(R.id.main_page_)
+        mAuth = FirebaseAuth.getInstance();
+
+        mlogout=(Button)findViewById(R.id.logoutbtn);
+
+        mlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                sendToAuth();
+            }
+        });
+
+    }
+
+    private void sendToAuth() {
+
+        Intent authIntent=new Intent(MainActivity.this,AuthActivity.class);
+        startActivity(authIntent);
+        finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser==null)
+        {
+                Intent mainIntent=new Intent(MainActivity.this,AuthActivity.class);
+                startActivity(mainIntent);
+                finish();
+        }
+
+    }
+}
